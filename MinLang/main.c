b20c224df577779725a60e5a37254ae4689ac59d
Ethan Webb <ethanmwebb@gmail.com>
2015-09-06 13:49:43 +0100

    Initial Commit

---
+++ MinLang/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MinLang
+//
+//  Created by Ethan Webb on 06/09/2015.
+//  Copyright (c) 2015 Ethan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
